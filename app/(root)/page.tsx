a5020019ae3ec6da5d30d87d9ee8dac52a4eec3f
Balapashka <eldos.sergali@mail.ru>
2024-05-06 16:08:14 +0500

    add profile sidebar and bank-card section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Yeldos" };
+  const loggedIn = {
+    firstName: "Yeldos",
+    lastName: "Sergali",
+    email: "max.boostvink@gmail.com",
+  };
 
   return (
     <section className="home">
@@ -21,7 +26,14 @@ const Home = () => {
             totalCurrentBalance={280940.77}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 820500.5 }, { currentBalance: 75200.2 }]}
+      />
     </section>
   );
 };
